a5ed985a30da5bb9fb76dbb591e2b9de63f21eea
Araam Zaremehrjardi <araamzaremehrjardi@gmail.com>
2022-01-27 10:57:14 -0800

    Added Folders for Backend API

--- opf/backend/api/__inti__.py
+++ opf/backend/api/__inti__.py
@@ -5,3 +5,4 @@ api.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://../database.db'
 api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 database = SQLAlchemy(api)
 
+# from api import models, routes
