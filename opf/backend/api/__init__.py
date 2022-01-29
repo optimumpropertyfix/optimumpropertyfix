3c3e34064dfdbb3a486a8c870af6e0d6e405761c
joannalopez223 <joannalopez@nevada.unr.edu>
2022-01-28 22:59:33 -0800

    fix import

--- opf/backend/api/__init__.py
+++ opf/backend/api/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 api = Flask(__name__)
-api.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://../database.db'
+api.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 database = SQLAlchemy(api)
 
