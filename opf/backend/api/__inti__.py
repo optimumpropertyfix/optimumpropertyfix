caaff7a2e10e6ffe4a268559ff3304947d0bd3b2
joannalopez223 <joannalopez@nevada.unr.edu>
2022-01-28 22:34:48 -0800

    change

--- opf/backend/api/__inti__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-api = Flask(__name__)
-api.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://../database.db'
-api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-database = SQLAlchemy(api)
-
-# from api import models, routes
