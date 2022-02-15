09c6c7a03437b61f90bcf75ff75f551d49c2bc5d
Araam Zaremehrjardi <araamzaremehrjardi@gmail.com>
2022-02-15 09:55:28 -0800

    Updated Styles

--- opf/src/pages/LoginPage/views/CreateAccountView/CreateAccountView.js
+++ opf/src/pages/LoginPage/views/CreateAccountView/CreateAccountView.js
@@ -1,7 +1,23 @@
+import styles from "./CreateAccountView.module.css";
 function CreateAccountView() {
   return (
-    <div>
-      <p>CreateAccountView</p>
+    <div className={`${styles.CreateAccountView} layout_helper_ParentSet`}>
+      <div className="layout_helper_ChildItem">
+        <p className="text_page_title">Create an Account</p>
+      </div>
+      <div className="layout_helper_ChildItem">
+        <form className={styles.form}>
+          <div className="layout_helper_FormGroup">
+            <label>Email Address</label>
+            <input type="text" />
+          </div>
+          <div className="layout_helper_FormGroup">
+            <label>NSHE ID</label>
+            <input type="text" />
+          </div>
+        </form>
+        <textarea></textarea>
+      </div>
     </div>
   );
 }
