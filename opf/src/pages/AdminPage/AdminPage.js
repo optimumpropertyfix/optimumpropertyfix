914a9733a8986408b6b40a302cfa7fec652894f5
Araam Zaremehrjardi <araamzaremehrjardi@gmail.com>
2022-02-16 20:07:18 -0800

    Updated Styles

--- opf/src/pages/AdminPage/AdminPage.js
+++ opf/src/pages/AdminPage/AdminPage.js
@@ -3,22 +3,43 @@ import {
   Navigation,
   NavigationLink,
 } from "../../components/Navigation/Navigation";
-
+import styles from "./AdminPage.module.css";
 function AdminPage() {
   return (
-    <div>
-      <Navigation>
-        <NavigationLink end to="/admin">
+    <div className={`${styles.AdminPage} page_helper_container`}>
+      <Navigation
+        className={`${styles.nav} page_navigation page_helper_NavigationSection`}
+      >
+        <NavigationLink
+          active_class="page_navigation_link_active"
+          className={"page_navigation_link"}
+          end
+          to="/admin"
+        >
           Dashboard
         </NavigationLink>
-        <NavigationLink to="maintenance_requests">
+        <NavigationLink
+          active_class="page_navigation_link_active"
+          className={"page_navigation_link"}
+          to="maintenance_requests"
+        >
           Maintenance Requests
         </NavigationLink>
-        <NavigationLink end to="feedback">
+        <NavigationLink
+          active_class="page_navigation_link_active"
+          className={"page_navigation_link"}
+          end
+          to="feedback"
+        >
           Feedback
         </NavigationLink>
       </Navigation>
-      <div>
+      <header
+        className={`${styles.header} page_header page_header page_helper_HeaderSection`}
+      >
+        test
+      </header>
+      <div className={`${styles.outlet} page_helper_OutletSection`}>
         <Outlet />
       </div>
     </div>
