329295dd1a959d6c89add0e2a31d4bc1d26f160e
joannalopez223 <joannalopez@nevada.unr.edu>
2022-03-02 13:28:48 -0800

    Editing

--- opf/src/pages/FormPage/views/AnnouncementView/AnnouncementView.js
+++ opf/src/pages/FormPage/views/AnnouncementView/AnnouncementView.js
@@ -1,49 +1,9 @@
-/* References: 
-[1] https://reactjs.org/docs/forms.html 
-[2] https://www.telerik.com/blogs/how-to-build-forms-with-react-the-easy-way 
-*/
-import React, { useState } from "react";
-import styles from "./AnnouncementView.module.css";
+import AnnouncementForm from "../../../../components/Forms/Announcement/AnnouncementForm";
 
-export function AdminCreateAnnouncementView() {
-  const [title, setTitle] = useState([]);
-  const [content, setContent] = useState([]);
-
-  const handleSubmit = (event) => {
-    console.log(`
-      Title:${title}
-      Content:${content}
-    `);
-    event.preventDefault();
-  };
-
+export const AdminCreateAnnouncementView = () => {
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <div className={styles.header}>
-        <h1>Announcement</h1>
-      </div>
-      <label>
-        Title:
-        <input
-          name="title"
-          type="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Content:
-        <input
-          name="content"
-          type="content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          required
-        />
-      </label>
-
-      <button className={styles.button}>Submit</button>
-    </form>
+    <div>
+      <AnnouncementForm />
+    </div>
   );
-}
+};
--- opf/src/pages/FormPage/views/FAQView/FAQView.js
+++ opf/src/pages/FormPage/views/FAQView/FAQView.js
@@ -1,49 +1,9 @@
-/* References: 
-[1] https://reactjs.org/docs/forms.html 
-[2] https://www.telerik.com/blogs/how-to-build-forms-with-react-the-easy-way 
-*/
-import React, { useState } from "react";
-import styles from "./FAQView.module.css";
+import FAQForm from "../../../../components/Forms/FAQ/FAQForm";
 
-export function AdminCreateFAQView() {
-  const [question, setQuestion] = useState([]);
-  const [answer, setAnswer] = useState([]);
-
-  const handleSubmit = (event) => {
-    console.log(`
-      Question:${question}
-      Answer:${answer}
-    `);
-    event.preventDefault();
-  };
-
+export const AdminCreateFAQView = () => {
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <div className={styles.header}>
-        <h1>Frequently Asked Question</h1>
-      </div>
-      <label>
-        Question:
-        <input
-          name="question"
-          type="question"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Answer:
-        <input
-          name="answer"
-          type="answer"
-          value={answer}
-          onChange={(e) => setAnswer(e.target.value)}
-          required
-        />
-      </label>
-
-      <button className={styles.button}>Submit</button>
-    </form>
+    <div>
+      <FAQForm />
+    </div>
   );
-}
+};
